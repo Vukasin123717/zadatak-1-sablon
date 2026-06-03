d0ae82944dbb7fe03f2449c4a3a6bfd99d947acd
Vukasin123717 <134974869+Vukasin123717@users.noreply.github.com>
2026-06-03 10:32:16 +0200

    Implement number summation function
    Added a function to sum two numbers with a test case.

---
+++ main.py
@@ -0,0 +1,8 @@
+def saberi_brojeve(a, b):
+    # TODO: Obriši 'pass' i napiši kod koji vraća zbir brojeva a i b
+    pass
+
+if __name__ == "__main__":
+    # Ovde testiramo kod
+    rezultat = saberi_brojeve(5, 3)
+    print(f"Zbir brojeva 5 i 3 je: {rezultat}")
